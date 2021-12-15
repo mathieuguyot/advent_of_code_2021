d1094dd79a5746459991bc91d972dde89a702926
Mathieu Guyot <mathieuguyot40@gmail.com>
2021-12-15 20:49:32 +0100

    Added day 14 part 2

--- day_14.hs
+++ day_14.hs
@@ -10,35 +10,43 @@ wordsWhen p s = case dropWhile p s of
         where (w, s'') = break p s'
 
 type Rule = (Char, Char, Char)
+type Occurence = ((Char, Char), Int)
 
-thrd :: Rule -> Char
-thrd (_, _, v) = v
+stepO :: Occurence -> [Rule] -> [Occurence]
+stepO ((a, b), n) rules = do
+    let filteredRules = filter (\(a2, b2, _) -> a2 == a && b2 == b) rules
+    if null filteredRules then [((a, b), n)] else do
+        let (_, _, c) = head filteredRules
+        [((a, c), n), ((c, b), n)]
+
+step2 :: [Occurence] -> [Rule] -> [Occurence]
+step2 os rules = groupOccurences $ concatMap (`stepO` rules) os
+
+step2N :: [Occurence] -> [Rule] -> Int -> [Occurence]
+step2N os _ 0 = os
+step2N os r n = step2N (step2 os r) r (n-1)
+
+groupOccurences :: [Occurence] -> [Occurence]
+groupOccurences [] = []
+groupOccurences (((a, b), n):os) = do
+    let os' = filter (\((a2, b2), _) -> a /= a2 || b /= b2) os
+    let same = filter (\((a2, b2), _) -> a == a2 && b == b2) os
+    let count = sum (map snd same) + n
+    ((a,b), count) : groupOccurences os'
+
+groupLetters :: [Occurence] -> [(Char, Int)]
+groupLetters [] = []
+groupLetters (((a, _), n):os) = do
+    let os' = filter (\((a2, _), _) -> a /= a2) os
+    let same = filter (\((a2, _), _) -> a == a2) os
+    let count = sum (map snd same) + n
+    (a, count) : groupLetters os'
 
 parseRules :: [String] -> [Rule]
 parseRules []     = []
 parseRules (x:xs) = do
     let ws = wordsWhen (==' ') x
     (head (head ws), last (head ws), head $ last ws) : parseRules xs
-
-getRule :: Char -> Char -> [Rule] -> String
-getRule a b rules = do
-    let fRules = filter (\(a2,b2,_) -> a == a2 && b == b2) rules
-    [thrd (head fRules) | not $ null rules]
-
-step :: String -> [Rule] -> String
-step str rules = concat $ zipWith (\a b -> a : getRule a b rules) str (tail str) ++ [[last str]]
-
-stepN :: String -> [Rule] -> Int -> IO()
-stepN str _ 0 = do
-    print 0
-    let sums = sort $ map length $ group $ sort str
-    print 0
-    print (maximum sums - minimum sums)
-stepN str rules n = do 
-    let sums = sort $ map length $ group $ sort str
-    print n
-    print (maximum sums - minimum sums)
-    stepN (step str rules) rules (n-1)
 
 main :: IO()
 main = do
@@ -47,7 +55,8 @@ main = do
     let cont = wordsWhen (=='\n') content
     let str = head cont
     let rules = parseRules $ tail cont
-    let s10 = stepN str rules 10
-    let s40 = stepN str rules 40
-    s10
-    s40
+    let occurences = map (\e -> (head e, length e)) (group $ sort $ zip str $ tail str) ++ [((last str, '#'), 1)]
+    let s10 = sort $ map snd $ groupLetters $ step2N occurences rules 10
+    print $ last s10 - head s10
+    let s40 = sort $ map snd $ groupLetters $ step2N occurences rules 40
+    print $ last s40 - head s40
